c7e622a16f66a0e2d8755c7676bf0e9fb3ce634d
nabrain <michal2002.60@gmail.com>
2025-11-07 11:07:56 +0100

    add

--- src/main/java/org/langapp/string/StringUtil.java
+++ src/main/java/org/langapp/string/StringUtil.java
@@ -3,6 +3,7 @@ package org.langapp.string;
 public class StringUtil {
     public String normalized(String input) {
         return input
+                .trim()
                 .toLowerCase()
                 .replaceAll("[^\\p{L}\\p{N}\\s]", "");
     }
